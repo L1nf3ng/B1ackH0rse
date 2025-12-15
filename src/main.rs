56006b7478332f6caa7f2f99fcfce97fc70a2897
L1nf3ng <nuaa_llf@126.com>
2025-12-16 00:46:35 +0800

    重要：增加http代理处置逻辑，并将原tokio服务转成hyper服务。

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
-use B1ackH0rse::network_engine::server::Server;
+use B1ackH0rse::config::Config;
+use B1ackH0rse::network_engine::server::proxy_services;
+
+use hyper::server::{Server, conn::AddrStream};
+use hyper::service::{make_service_fn, service_fn};
+use std::net::SocketAddr;
 use std::error::Error;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>>{
-    let server = Server::default();
-    server.start().await?;
-    println!("Received NULL character, so we ended server!");
+    // 初始化日志
+    env_logger::init();
+
+    // todo!增加从命令行参数或配置文件读配置的逻辑。
+    let config: Config = Config::default();
+    // 这里我们切换成hyper server
+    println!("Starting server at {}:{}", config.address, config.port);
+    let addr = SocketAddr::from((config.ip, config.port));
+    let make_mvc = make_service_fn(move |_conn: &AddrStream|{
+        let remote = _conn.remote_addr();
+        async move{
+            Ok::<_, hyper::Error>(service_fn(move |req| {
+                // 在这个异步函数里写处理逻辑，成功时返回Response Body内容
+                proxy_services(req,remote)
+            }))
+        }
+    });
+    let server = Server::bind(&addr).serve(make_mvc);   
+        // 优雅关闭
+    if let Err(e) = server.await {
+        eprintln!("服务器异常退出: {}", e);
+    }
     Ok(())
+
 }
--- src/network_engine/server.rs
+++ src/network_engine/server.rs
@@ -1,114 +1,64 @@
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::{TcpListener, TcpStream};
-use std::collections::HashMap;
-use std::error::Error;
+use hyper::{Body, Client, Method, Request, Response};
+use std::net::SocketAddr;
 
-pub struct Server{
-    address: String,
-    port: u32,
-    workers: u8
-}
 
-impl Default for Server{
-    fn default() -> Self {
-        Server { 
-            //tcp层监听，但仅解析https、http、websocke三种协议，其中https需要安装证书并解密。
-            address: "localhost".to_string(), 
-            port: 8090, 
-            workers: 1 
+pub async fn proxy_services(_req: Request<Body>, remote: SocketAddr ) -> Result<Response<Body>,  hyper::Error> {
+    //初始化一个http客户端用来做转发用
+    let client= hyper::Client::new();
+
+    match _req.method() {
+        &Method::CONNECT => {
+            // 对于HTTPS来说开启Connect隧道，制作字节转发
+            println!("Received HTTPS CONNECT request from {}", remote);
+                // 返回响应
+            Ok(Response::new(Body::from("Hello from B1ackH0rse proxy server!")))
+        },
+        _ => {
+            // 别的HTTP请求解析并转发
+            println!("Received HTTP request from {}", remote);
+            handle_http_requests(_req, client).await
         }
-    }
-
-}
-
-
-impl Server{
-    pub fn new( port: u32, addr: String, workers: u8) -> Self{
-        Self{
-            address: addr,
-            port: port,
-            workers: workers
-        }
-    }
-
-    pub async fn start(&self) -> Result<(), Box<dyn Error>>{
-        let bind_str = format!("{}:{}", self.address, self.port);
-        let server = TcpListener::bind(bind_str).await?;
-        
-        loop {
-            let mut res: String = String::new();
-            let (mut socket, cli_addr) = server.accept().await?;
-            loop{
-                let mut buffer = [0;4098];      // 4KB unit
-                if let Ok(n) = socket.read(&mut buffer).await {
-                    if n == 0 {
-                        break
-                    }
-                    println!("The request comes from {cli_addr}");
-                    let message = String::from_utf8_lossy(&buffer[..n]);
-                    res.push_str(&message);
-                }
-                else{
-                    eprintln!("we received some wrong data!");
-                    break;
-                }
-            }
-            println!("we received following data:\n {res}");
-            // 在这里处理请求，对的。
-            // 在这里完成目标地址改写，也就是代理作为中继，需要按照Host值做请求的重新发送
-            if res.contains("HTTP") {
-                self.handle_http_requests(res).await?;
-                self.forward_request().await?;
-                self.forward_response().await?;
-
-            }
-
-
-            let response:String = format!("200\r\n\r\nConnection: Close\r\n\r\n我们已收到你的长度为的请求");
-            socket.write_all(response.as_bytes()).await;
-            break;
-        }
-        Ok(())
-    }
-
-
-    pub async fn handle_http_requests(&self, request_str: String) -> Result<(), Box<dyn Error>> {
-        let mut request_lines = request_str.lines();
-
-        if let Some(first_line) = request_lines.next(){
-            let first_row:Vec<&str> = first_line.split_whitespace().collect();
-            if first_row.len()<3 {
-                panic!("the http request has a bad format!");
-            }
-            let method = first_row[0];
-            let uri = first_row[1];
-            let protocol = first_row[2];
-            println!("the request protocal is {protocol}; method {method}; uri {uri}");
-            let mut header_map: HashMap<&str, &str> = HashMap::new();
-            for line in request_lines{
-                if let Some(pos_idx) = line.find(":"){
-                    let key = line[..pos_idx].trim();
-                    let value = line[pos_idx+1..].trim();
-                    header_map.insert(key, value);
-                }
-            }
-            println!("the requset headers are: {:?}", header_map);
-
-        }
-
-        Ok(())
-    }
-
-
-    pub async fn forward_request(&self) -> Result<(), &'static str>{
-        ///这个函数将source的请求发送给target
-        todo!();
-    }
-
-
-    pub async fn forward_response(&self) -> Result<(), &'static str>{
-        ///这个函数将收到的server端响应转发给source
-        todo!();
     }
 }
 
+
+pub async fn handle_http_requests(req: Request<Body>, client: Client<hyper::client::HttpConnector>) -> Result<Response<Body>, hyper::Error> {
+    let target = req.uri();
+    println!("Forwarding request to target: {}", target);
+
+    let mut request = Request::builder()
+                            .method(req.method())
+                            .uri(target)
+                            .version(req.version());
+
+    let filter_header_list = vec!["Connection" ,"Keep-Alive", "Proxy-Authenticate" ,"Proxy-Authorization",
+        "Te" ,"Trailer","Transfer-Encoding" ,"Upgrade"];
+    // 解析请求头并过滤打断链接的头
+    for (key, value) in req.headers().iter(){
+        if filter_header_list.contains(&key.as_str()) {
+            continue;
+        }
+        else{
+            request = request.header(key, value);
+        }
+    }
+
+    // 拷贝请求体，这里采用遮蔽的方式将Builder结构体转换成了Request结构体
+    let request = request.body(req.into_body()).expect("Failed to build request");
+
+    // 用client发送请求
+    let resp = client.request(request).await?;
+    let mut response = Response::builder()
+        .status(resp.status())
+        .version(resp.version());
+
+    // 复制响应头
+    for (key, value) in resp.headers().iter(){
+        response = response.header(key, value);
+    }
+
+    // 复制响应体
+    let response = response.body(resp.into_body()).expect("Failed to build response");
+
+    Ok(response)
+}
