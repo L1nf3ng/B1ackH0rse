905aed87aa890eb90c29a4143c623a7f5ff701a0
L1nf3ng <nuaa_llf@126.com>
2025-12-22 23:51:13 +0800

    add basic generate and load certs functions.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,11 @@ use B1ackH0rse::config::Config;
 use B1ackH0rse::network_engine::server::proxy_services;
 use hyper::server::{Server, conn::AddrStream};
 use hyper::service::{make_service_fn, service_fn};
-use std::net::SocketAddr;
 use std::error::Error;
-
+use std::net::SocketAddr;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>>{
+async fn main() -> Result<(), Box<dyn Error>> {
     // 初始化日志
     env_logger::init();
 
@@ -16,20 +15,19 @@ async fn main() -> Result<(), Box<dyn Error>>{
     // 这里我们切换成hyper server
     println!("Starting server at {}:{}", config.address, config.port);
     let addr = SocketAddr::from((config.ip, config.port));
-    let make_mvc = make_service_fn(move |_conn: &AddrStream|{
+    let make_mvc = make_service_fn(move |_conn: &AddrStream| {
         let remote = _conn.remote_addr();
-        async move{
+        async move {
             Ok::<_, hyper::Error>(service_fn(move |req| {
                 // 在这个异步函数里写处理逻辑，成功时返回Response Body内容
-                proxy_services(req,remote)
+                proxy_services(req, remote)
             }))
         }
     });
-    let server = Server::bind(&addr).serve(make_mvc);   
-        // 优雅关闭
+    let server = Server::bind(&addr).serve(make_mvc);
+    // 优雅关闭
     if let Err(e) = server.await {
         eprintln!("服务器异常退出: {}", e);
     }
     Ok(())
-
 }
---
+++ src/utils/cert.rs
@@ -0,0 +1,35 @@
+use std::{path::Path, fs};
+use rcgen::{Certificate, KeyPair, CertificateParams, IsCa, BasicConstraints, Issuer};
+use std::error::Error;
+
+
+/// 参考文档：https://blog.csdn.net/yuan__once/article/details/137635953
+/// 加载已经生成的证书进程序
+pub fn load_cert() -> Result<(Issuer<'_>, KeyPair), &'static str>{
+    if Path::new("ca.pem").exists() && Path::new("prikey.pem").exists() {
+        let ca_str = fs::read_to_string("ca.pem").unwrap();
+        let key_str = fs::read_to_string("prikey.pem").unwrap();
+        // TODO: 不兼容，需检索正确的加载方式。
+        let key = KeyPair::from_pem(&key_str).unwrap();
+        // 官方用法：https://github.com/rustls/rcgen/blob/b250fa36a553a7d090d36ad70886c97191065581/rcgen/src/certificate.rs#L1362
+        let ca = Issuer::from_ca_cert_pem(&ca_str,ca_kp).unwrap();
+        Ok((ca, key))
+    }
+    else{
+        return Err("either ca.pem or it's private-key not exists, Generate one please.")
+    }
+}
+
+/// 在没有证书的情况下使用命令行参数可以生成一个证书，客户端需安装它并选择信任它。
+pub fn generate_cert() -> Result<(String, String), Box<dyn Error> >{
+    let mut params = CertificateParams::default();
+    params.is_ca = IsCa::Ca(BasicConstraints::Unconstrained);
+    if let Ok(cert) = Certificate::from_params(params){
+        let cert_pem = cert.serialize_pem().unwrap();
+        let prikey_pem = cert.serialize_private_key_pem();
+        Ok((cert_pem, prikey_pem))
+    }
+    else{
+        panic!("Error in creating certifications!");
+    }
+}
