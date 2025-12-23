30e0451ec2cd9b23113ac38b3e1bc00b5445c897
L1nf3ng <nuaa_llf@126.com>
2025-12-24 00:11:32 +0800

    新增生成证书文件功能及单元测试。

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,26 @@ pub mod config;
 #[cfg(test)]
 mod tests{
 
+    use crate::utils::cert;
+
     use super::network_engine::learn_hyper::minimal_hyper_server;
+    use super::utils::cert::generate_cert;
+    use std::fs;
 
     #[tokio::test]
     async fn test_minimal_server(){
         assert_eq!(minimal_hyper_server().await, ());
+    }
+
+    #[tokio::test]
+    async fn test_certifactes(){
+        match generate_cert() {
+            Ok((cert_str, key_str)) => {
+                fs::write("cert.pem", cert_str).unwrap();
+                fs::write("prikey.pem", key_str).unwrap();
+            },
+            _ => {}
+        }
+
     }
 }
